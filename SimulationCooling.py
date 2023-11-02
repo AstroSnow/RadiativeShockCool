67335e8c54bee26b7d073d18cea319820884356c
snow <b.snow@exeter.ac.uk>
2023-11-02 12:08:20 +0000

    Colours for the histogram

--- SimulationCooling.py
+++ SimulationCooling.py
@@ -10,67 +10,110 @@ import matplotlib.pyplot as plt
 import numpy as np
 import shockid
 import h5py
+from matplotlib import colors
 
-fname = '/media/ben/SnowM2/mhd_rad/'
-ds=pipread(fname,40) #Get the simulation data
+#fname = '/media/ben/SnowM2/mhd_rad/'
+fname = '/media/snow/SnowM2/mhd_rad/'
+
+ShockJumpRead=True
+ContextPlot=False
+
+if ContextPlot==True:
+	ds=pipread(fname,40) #Get the simulation data
+	#Create a h5 file of the shock data
+	shocks=shockid.restoreShocks(''.join((fname,'shocks.h5')))
+	shocksf=shockid.shockFilter(shocks, 3)
+	
+	xg=ds['xgrid'][shocks['xs']:shocks['xe']]
+	yg=ds['ygrid'][shocks['ys']:shocks['ye']]
+	ro=ds['ro_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	pr=ds['pr_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	vx=ds['vx_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	vy=ds['vy_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	vz=ds['vz_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	bx=ds['bx'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	by=ds['by'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	bz=ds['bz'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
+	
+	ds2={}
+	ds2['xgrid']=xg
+	ds2['ygrid']=yg
+	ds2['ro']=ro
+	ds2['vx']=vx
+	ds2['vy']=vy
+	ds2['vz']=vz
+	ds2['bx']=bx
+	ds2['by']=by
+	ds2['bz']=bz
+	ds2['pr']=pr
+	
+	
+	fig, ax = plt.subplots(figsize=(9, 6))
+	plt.contourf(np.log10(ro),levels=101,cmap='Greys')
+	sl=ax.plot(shocksf['slow'][:,1],shocksf['slow'][:,0],color='r',linestyle='',marker='.',markersize=2.8)
+	ax.set_xlabel('x')
+	ax.set_ylabel('y')
+	plt.savefig('shockCoolingTest_context.png',dpi=300)
 
 
-#Create a h5 file of the shock data
-shocks=shockid.restoreShocks(''.join((fname,'shocks.h5')))
-shocksf=shockid.shockFilter(shocks, 3)
+if ShockJumpRead == True:
+	with h5py.File(''.join((fname,'ShockJumpData.h5')), "r") as f:
+		# Print all root level object names (aka keys) 
+		# these can be group or dataset names 
+		print("Keys: %s" % f.keys())
+		# get first object name/key; may or may NOT be a group
+		a_group_key = list(f.keys())[0]
 
-xg=ds['xgrid'][shocks['xs']:shocks['xe']]
-yg=ds['ygrid'][shocks['ys']:shocks['ye']]
-ro=ds['ro_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-pr=ds['pr_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-vx=ds['vx_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-vy=ds['vy_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-vz=ds['vz_p'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-bx=ds['bx'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-by=ds['by'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-bz=ds['bz'][shocks['ys']:shocks['ye'],shocks['xs']:shocks['xe']]
-
-ds2={}
-ds2['xgrid']=xg
-ds2['ygrid']=yg
-ds2['ro']=ro
-ds2['vx']=vx
-ds2['vy']=vy
-ds2['vz']=vz
-ds2['bx']=bx
-ds2['by']=by
-ds2['bz']=bz
-ds2['pr']=pr
-
-
+	# get the object type for a_group_key: usually group or dataset
+		print(type(f[a_group_key])) 
+	
+	    # If a_group_key is a group name, 
+	    # this gets the object names in the group and returns as a list
+		Tjarr = list(f['Tjarr'])
+		rojarr=list(f['rojarr'])
+		prjarr=list(f['prjarr'])
+else:	
+	Tjarr=[]
+	T2jarr=[]
+	rojarr=[]
+	prjarr=[]
+	for tpoint in range(0,np.size(shocksf['slow'][:,0])):
+	
+		print(tpoint,'/',np.size(shocksf['slow'][:,0]))
+		shockLine=shockid.shockLine(shocksf['slow'][tpoint,:], ds2)
+		ipre,ipos=shockid.prepostIndex(shockLine['normarr']['ro'],5)
+		
+		T=shockLine['normarr']['pr']/shockLine['normarr']['ro']
+		
+		Tj=T[ipos]/T[ipre]
+		T2j=(T[ipos]-T[ipre])/T[ipre]
+		roj=shockLine['normarr']['ro'][ipos]/shockLine['normarr']['ro'][ipre]
+		prj=shockLine['normarr']['pr'][ipos]/shockLine['normarr']['pr'][ipre]
+		
+		Tjarr.append(Tj)
+		T2jarr.append(T2j)
+		rojarr.append(roj)
+		prjarr.append(prj)
+	
+	#Save the Data	
+	hf = h5py.File(''.join((fname,'ShockJumpData.h5')), 'w')
+	hf.create_dataset('Tjarr', data=Tjarr)
+	hf.create_dataset('T2jarr', data=T2jarr)
+	hf.create_dataset('rojarr', data=rojarr)
+	hf.create_dataset('prjarr', data=prjarr)
+	hf.close()
+	
 fig, ax = plt.subplots(figsize=(9, 6))
-plt.contourf(np.log10(ro),levels=101,cmap='Greys')
-sl=ax.plot(shocksf['slow'][:,1],shocksf['slow'][:,0],color='r',linestyle='',marker='.',markersize=2.8)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-plt.savefig('shockCoolingTest_context.png',dpi=300)
-
-Tjarr=[]
-rojarr=[]
-for tpoint in range(0,np.size(shocksf['slow'][:,0])):
-
-	print(tpoint)
-	shockLine=shockid.shockLine(shocksf['slow'][tpoint,:], ds2)
-	ipre,ipos=shockid.prepostIndex(shockLine['normarr']['ro'],5)
-	
-	T=shockLine['normarr']['pr']/shockLine['normarr']['ro']
-	
-	Tj=T[ipos]/T[ipre]
-	roj=shockLine['normarr']['ro'][ipos]/shockLine['normarr']['ro'][ipre]
-	
-	Tjarr.append(Tj)
-	rojarr.append(roj)
-	
-fig, ax = plt.subplots(figsize=(9, 6))
-ax.hist(Tjarr,bins=np.linspace(0,14,150))
-ax.plot([1,1],[0,600],'r')
-ax.set_xlim(0,15)
-ax.set_ylim(0,600)
-ax.set_xlabel('$T^d/T^u$')
+#plt.hist(np.log10(Tjarr),bins=np.linspace(-2,2,150))
+Tjar=np.array(Tjarr)
+ax.hist(np.log10(Tjar),bins=np.linspace(-2,2,150),color='g')
+ax.hist(np.log10(Tjar[np.where(Tjar<0.9)]),bins=np.linspace(-2,2,150))
+ax.hist(np.log10(Tjar[np.where(Tjar>1.1)]),bins=np.linspace(-2,2,150),color='r')
+#ax.plot([0,0],[0,600],'r')
+ax.set_xlim(-2,2)
+ax.set_ylim(0,210)
+ax.set_xlabel('$\log_{10}(T^d/T^u)$')
 ax.set_ylabel('Counts')
 plt.savefig('shockCoolingTest.png',dpi=300)
+
+#ax.scatter((rojarr),np.log10(Tjarr),c=np.log10(prjarr),norm=colors.CenteredNorm(),cmap='RdBu');plt.colorbar()
